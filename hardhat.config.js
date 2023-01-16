4a63aec702d84ae6f376d8042132409c0f953fe1
Salomão Nunes <reinaldo.vombo@gmail.com>
2023-01-16 18:09:16 +0000

    add env file

--- hardhat.config.js
+++ hardhat.config.js
@@ -2,11 +2,19 @@
 module.exports = {
   solidity: {
     version: '0.8.9',
+    defaultNetwork: 'goerli',
+    networks: {
+      hardhat: {},
+      goerli: {
+        url: 'https://rpc.ankr.com/eth',
+        accounts: [`0x${process.env.PRIVATE_KEY}`]
+      }
+    },
     settings: {
       optimizer: {
         enabled: true,
-        runs: 200,
-      },
-    },
-  },
-};
+        runs: 200
+      }
+    }
+  }
+}
